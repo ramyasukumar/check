fc575a64c4da882f6c27725dd82d63bd217096f8
ramyasukumar <33746459+ramyasukumar@users.noreply.github.com>
2017-12-05 14:53:42 +0530

    Update HelloWorldController.java

--- api-ai-java-webhook-master/src/main/java/hello/HelloWorldController.java
+++ api-ai-java-webhook-master/src/main/java/hello/HelloWorldController.java
@@ -11,10 +11,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloWorldController {
 
     @RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
+public @ResponseBody WebhookResponse webhook(@RequestBody WebhookRequest webhookRequest){
 
-        System.out.println(obj);
+        String percentage =webhookRequest.getResolvedQuery();
 
-        return new WebhookResponse("Hello! "+ obj, "Text " + obj);
+        int value = Integer.parseInt(percentage.replaceAll("[^0-9]", ""));
+
+        String speech ;
+        String display_text;
+        if (value >75)
+        {
+         speech="congrats ! you are eligible";
+            display_text="congrats ! you are eligible";
+        }
+        else
+        {
+            speech ="sorry ! you are not eligible you can try another course";
+            display_text="sorry ! you are not eligible you can try another course";
+        }
+
+       // System.out.println(obj);
+
+        return new WebhookResponse(speech, display_text);   
     }
 }
