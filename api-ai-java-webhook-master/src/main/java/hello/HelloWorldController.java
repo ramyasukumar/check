0d11ec72d406aab23dd7c425089e8506ab57a62b
ramyasukumar <33746459+ramyasukumar@users.noreply.github.com>
2017-12-05 15:51:17 +0530

    Update HelloWorldController.java

--- api-ai-java-webhook-master/src/main/java/hello/HelloWorldController.java
+++ api-ai-java-webhook-master/src/main/java/hello/HelloWorldController.java
@@ -18,20 +18,20 @@ public @ResponseBody WebhookResponse webhook(@RequestBody WebhookRequest webhook
         int value = Integer.parseInt(percentage.replaceAll("[^0-9]", ""));
 
         String speech ;
-        String display_text;
+      //  String display_text;
         if (value >75)
         {
          speech="congrats ! you are eligible";
-            display_text="congrats ! you are eligible";
+            //display_text="congrats ! you are eligible";
         }
         else
         {
             speech ="sorry ! you are not eligible you can try another course";
-            display_text="sorry ! you are not eligible you can try another course";
+           // display_text="sorry ! you are not eligible you can try another course";
         }
 
        // System.out.println(obj);
 
-        return new WebhookResponse(speech, display_text);   
+        return new WebhookResponse(speech, "text");   
     }
 }
